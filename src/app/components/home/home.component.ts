d84cbf993c6848c2b00f87e5b25123c5761422cf
phaetton <31552767+phaetton@users.noreply.github.com>
2022-01-28 14:16:20 -0600

    errores en spotify

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,15 +10,24 @@ export class HomeComponent implements OnInit {
 
   nuevasCanciones: any[] = [];
   loading: boolean;
+  error: boolean;
+  mensajeError: any;
 
   constructor(private spotifySvc: SpotifyService) {
     this.loading = true;
+    this.error = false;
 
     this.spotifySvc.getNewReleases()
       .subscribe((data: any) => {
         this.nuevasCanciones = data;
         this.loading = false;
-      });
+      },
+        (err) => {
+          this.loading = false;
+          this.error = true;
+          this.mensajeError = err.error.error.message;
+
+        });
   }
 
   ngOnInit(): void {
